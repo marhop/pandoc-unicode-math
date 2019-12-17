2918321ca2ea66c43bb2269fdc760715b5b39f16
Martin Hoppenheit <martin@hoppenheit.info>
2019-12-17 22:28:14 +0100

    Add sensible whitespace where necessary
    Closes #6

--- example.md
+++ example.md
@@ -3,16 +3,16 @@
 $λ$ terms:
 
   * Variable: $x$
-  * Abstraction: $(λ x . M)$
-  * Application: $(M N)$
+  * Abstraction: $(λx.M)$
+  * Application: $(MN)$
 
 Reduction operations:
 
-  * $α$ reduction: $λ x . M → λ y . [y/x]M$
-  * $β$ reduction: $(λ x . M)N → [N/x]M$
+  * $α$ reduction: $λx.M → λy.[y/x]M$
+  * $β$ reduction: $(λx.M)N → [N/x]M$
 
 Church numerals:
 
-  * $0 := λ fx . x$
-  * $1 := λ fx . fx$
-  * $2 := λ fx . f(fx)$
+  * $0 := λfx.x$
+  * $1 := λfx.fx$
+  * $2 := λfx.f(fx)$
--- src/UnicodeToLatex.hs
+++ src/UnicodeToLatex.hs
@@ -1,6 +1,7 @@
 module UnicodeToLatex where
 
-import Data.Map.Strict (findWithDefault)
+import Data.Char (isAlphaNum)
+import Data.Map.Strict ((!?), findWithDefault)
 import Text.Pandoc.JSON (toJSONFilter)
 
 import MathFilter
@@ -15,5 +16,21 @@ main = toJSONFilter (mathFilter unicodeToLatex)
 --   * α → \alpha
 --   * ℕ → \mathbb{N}
 --   * Α → A (greek Alpha to latin A), ugly but that's how Latex handles it
+--
+-- Sensible whitespace is added where necessary:
+--
+--   * λx → \lambda x
+--   * αβ → \alpha\beta
 unicodeToLatex :: String -> String
-unicodeToLatex = concatMap (\x -> findWithDefault [x] x unicodeToLatexMap)
+unicodeToLatex = foldr f ""
+  where
+    f :: Char -> String -> String
+    f x "" = findWithDefault [x] x unicodeToLatexMap
+    f x (y:ys) =
+        case unicodeToLatexMap !? x of
+            Just x' -> x' ++ isolate y ++ ys
+            Nothing -> x : y : ys
+    isolate :: Char -> String
+    isolate x
+        | isAlphaNum x = ' ' : [x]
+        | otherwise = [x]
